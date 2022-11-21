0dd0426b210a7d941fc6c8c4c7d00b06e32ac1db
fadhlanhasyim <fadhlanhasyim54@gmail.com>
2022-11-21 19:21:03 +0700

    dhafin mau liat warna

--- trigger_6/urls.py
+++ trigger_6/urls.py
@@ -5,4 +5,10 @@ app_name = 'trigger_6'
 
 urlpatterns = [
     path('', show_riwayat, name='show_riwayat'),
+    path('detail/<id>', show_detail_riwayat, name='show_detail_riwayat'),
+    path('penilaian/<id>', show_form_penilaian, name='show_form_penilaian'),
+    path('buat-promo/', show_buat_promo, name='show_buat_promo'),
+    path('buat-promo-minimum/', show_form_promo_minimum, name='show_form_promo_minimum'),
+    path('buat-promo-hari-spesial/', show_form_promo_hari_spesial, name='show_form_promo_hari_spesial'),
+    path('daftar-promo/', show_daftar_promo, name='show_daftar_promo'),
 ]
