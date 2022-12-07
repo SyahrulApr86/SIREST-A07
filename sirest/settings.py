5ae894501ffc6a6370790f7aa4f8d175dbfc91a4
Dhafin Raditya Juliawan <dhafinraditya35@gmail.com>
2022-12-07 16:48:08 +0700

    Add triggr 3

--- sirest/settings.py
+++ sirest/settings.py
@@ -10,12 +10,20 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-from pathlib import Path
+# from pathlib import Path
+# import os
+
+# # Build paths inside the project like this: BASE_DIR / 'subdir'.
+# BASE_DIR = Path(__file__).resolve().parent.parent
+
 import os
+import dj_database_url
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+PRODUCTION = os.getenv('DATABASE_URL') is not None
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -80,12 +88,7 @@ WSGI_APPLICATION = 'sirest.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = {}
 
 
 # Password validation
--- trigger3/views.py
+++ trigger3/views.py
@@ -1,6 +1,47 @@
-from django.shortcuts import render
+from django.db import connection
+from django.contrib import messages
+from django.shortcuts import render, redirect
 
 # Create your views here.
+
+def dictfetchall(cursor):
+    columns = [col[0] for col in cursor.description]
+    return [
+        dict(zip(columns, row))
+        for row in cursor.fetchall()
+    ]
+
+# TODO : ACTIVATE TRIGGER
+def tambah_tarif(request):
+    # if not is_authenticated(request) :
+    #     return redirect("/")
+    # else:
+    #     role = request.session["role"]
+    #     if role != 'admin':
+    #         return redirect("/")
+
+    if request.method == "POST":
+        # try :
+            
+            with connection.cursor() as cursor:
+                cursor.execute(f""" 
+                    INSERT INTO DELIVERY_FEE_PER_KM VALUES
+                    ('{request.POST['id_trf']}',
+                    '{request.POST['province']}',
+                    '{request.POST['motorfee']}',
+                    '{request.POST['carfee']}')
+                """)
+            messages.add_message(request, messages.SUCCESS, 'Tarif Pengiriman Berhasil Ditambahkan')
+            return redirect("/daftartarif")
+        
+
+    with connection.cursor() as c:
+        c.execute(f""" 
+            SELECT MAX(CAST(id as DECIMAL)) from delivery_fee_per_km;
+        """)
+        maxId = c.fetchall()[0][0]
+        length = int(maxId) + 1
+    return render(request, 'create_tarif_pengiriman.html', {'length': length})
 
 def tarif_detail(request):
     context = {
@@ -20,14 +61,14 @@ def makanan_detail(request):
     # }
     return render(request, "read_makanan.html")
     
-def tambah_tarif(request):
-    # context = {
-    #     'nomor' : '1',
-    #     'provinsi' : 'DKI Jakarta',
-    #     'motor' : '6000',
-    #     'mobil' : '7000',
-    # }
-    return render(request, "create_tarif_pengiriman.html")
+# def tambah_tarif(request):
+#     # context = {
+#     #     'nomor' : '1',
+#     #     'provinsi' : 'DKI Jakarta',
+#     #     'motor' : '6000',
+#     #     'mobil' : '7000',
+#     # }
+#     return render(request, "create_tarif_pengiriman.html")
 
 def tambah_makanan(request):
     # context = {
