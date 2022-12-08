ff791f8d5ab30d55ff141d199b5fb35ad4088fd6
HP <fernandonathanielsutanto@gmail.com>
2022-12-08 22:32:48 +0700

    Add sementara

--- trigger5/urls.py
+++ trigger5/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('detailpemesanankurir/', detail_pemesanan_kurir, name='detail_pemesanan_kurir'),
     path('buatbahanmakanan/', buat_bahanmakanan, name='buat_bahanmakanan'),
     path('daftarbahanmakanan/', daftar_bahanmakanan, name='daftar_bahanmakanan'),
+    path('daftarkategori/hapus-kategori/<int:id>', hapus_kategori, name='hapus_kategori'),
 ]
--- trigger5/views.py
+++ trigger5/views.py
@@ -1,12 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import *
+from django.urls import reverse
+from django.http import HttpResponse, HttpResponseRedirect
+from account.forms import *
+from utils.query import *
 
 # Create your views here.
 
 def buat_kategori(request):
+    if request.method == 'POST':
+        new_kategori = request.POST.get('kategori')
+        cursor.execute(f'select * restaurant_category')
+        id = cursor.fetchmany() + 1
+
+        cursor.execute(f""" 
+            INSERT INTO RESTAURANT_CATEGORY VALUES
+            ('{id}',
+            '{request.POST.['kategori']}')
+        """)
+
+        return redirect('trigger5:daftar_kategori')
+
     return render(request, "buat_kategori.html")
 
 def daftar_kategori(request):
-    return render(request, "daftar_kategori.html")
+    cursor.execute(f""" 
+        SELECT * from restaurant_category;
+    """)
+
+    data_kategori_resto = cursor.fetchall()
+
+    context = {
+        'list_kategori_resto': data_kategori_resto,
+    }
+    return render(request, "daftar_kategori.html", context)
 
 def pemesanan_kurir(request):
     return render(request, "pemesanan_kurir.html")
@@ -18,4 +44,8 @@ def buat_bahanmakanan(request):
     return render(request, "buat_bahanmakanan.html")
 
 def daftar_bahanmakanan(request):
-    return render(request, "daftar_bahanmakanan.html")
+    return render(request, "daftar_bahanmakanan.html")    
+
+def hapus_kategori(request,id):
+    return redirect('trigger5:daftar_kategori')
+
