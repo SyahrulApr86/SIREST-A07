99eaf817661e9af80d2902b8a4b152797b7e2027
fadhlanhasyim <fadhlanhasyim54@gmail.com>
2022-12-13 21:53:27 +0700

    Add Module sirest

--- sirest/settings.py
+++ sirest/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sirest',
     'account',
     'trigger_6',
     'trigger3',
